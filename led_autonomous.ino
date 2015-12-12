9c5baf3cee33d58bd983d1b3eb02bc6324170b8c
Daniel Bücheler <dbahn25@t-online.de>
2015-12-12 21:57:38 +0100

    changed fading to logarithmic (subjective linear) and fixed integer overflow when LIGHT_DELAY ist longer than a few seconds

--- led_autonomous.ino
+++ led_autonomous.ino
@@ -6,17 +6,19 @@ const int LED_PIN = 9; // Pin the transistor is connected to // pin 9 (PB1) with
 const int PIR_PIN = 2; // Pin the PIR motion sensor is connected to
 const int LDR_PIN = 3; // Pin the LDR light sensor is connected to
 
-const int LIGHT_DELAY_MS = 10000; // time for which the light shall stay on when a motion is detected
-const int FADE_DELAY_MS = 10; // _delay_ms at each step of fading
+const long LIGHT_DELAY_MS = 5 * 60000L; // time for which the light shall stay on when a motion is detected (millis)
+const long FADE_DELAY_MS = 50L; // _delay_ms at each step of fading
 
-const byte ON_VALUE = 255;
+const byte ON_VALUE = 31;
 const byte OFF_VALUE = 0;
 
 int current_brightness = 0;
 unsigned long light_on_ms = 0L;
 
+const uint8_t pwm_table[32] = {0, 1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 8, 9, 11, 13, 16, 19, 23, 27, 32, 38, 45, 54, 64, 76, 90, 107, 128, 152, 180, 214, 255};
+
 void setup() {
-    Serial.begin(9600);
+    //Serial.begin(9600);
 
     // pin direction definitions
     pinMode(PIR_PIN, INPUT);
@@ -33,7 +35,8 @@ void loop(){
         fade(ON_VALUE);
         light_on_ms = millis();
     } else if(millis() - light_on_ms < LIGHT_DELAY_MS) {
-        Serial.println("waiting");
+        Serial.print("waiting ");
+        Serial.println(LIGHT_DELAY_MS - (millis() - light_on_ms));
     }
     if(millis() - light_on_ms >= LIGHT_DELAY_MS) {
         fade(OFF_VALUE);
@@ -48,14 +51,14 @@ void fade(int target_brightness_level) {
         return;
     } else if(target_brightness_level < current_brightness) { // fade down
         while(current_brightness > target_brightness_level) {
-            analogWrite(LED_PIN, --current_brightness); // increase current_brightness and apply to register
-            Serial.println(current_brightness);
+            analogWrite(LED_PIN, pwm_table[--current_brightness]); // increase current_brightness and apply to register
+            Serial.println(pwm_table[current_brightness]);
             _delay_ms(FADE_DELAY_MS);
         }
     } else if(target_brightness_level > current_brightness) { // fade up
         while(current_brightness < target_brightness_level) {
-            analogWrite(LED_PIN, ++current_brightness); // increase current_brightness and apply to register
-            Serial.println(current_brightness);
+            analogWrite(LED_PIN, pwm_table[++current_brightness]); // increase current_brightness and apply to register
+            Serial.println(pwm_table[current_brightness]);
             _delay_ms(FADE_DELAY_MS);
         }
     }
